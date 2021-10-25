5223fd5157084c71ebb41661f3d20a1db23a75b2
iamheejin <adkj777@gmail.com>
2021-10-25 17:09:46 +0900

    전화번호 입력창에서 부모 Component로 데이터 전달

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React, {Component} from 'react';
 import PhoneForm from './components/PhoneForm'
 
 class App extends Component{
+  id = 2;
+  state = {
+    information: [
+      {
+        id:0,
+        name:'홍길동',
+        phone:'010-2222-3333'
+      },
+      {
+        id:1,
+        name:'김철수',
+        phone:'010-1234-3333'
+      }
+    ]
+  }
   handleCreate = (data) => {
     console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({id:this.id++, ...data})
+    })
   }
   render(){
     return (
       <div>
+        {this.props.name}
         <PhoneForm onCreate={this.handleCreate}/>
+        {JSON.stringify(this.state.information)}
       </div>
     )
   }
--- src/components/PhoneForm.js
+++ src/components/PhoneForm.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 
 class PhoneForm extends Component {
+  
+  state = {
+    name: '',
+    phone: ''
+  }
   handleChange = (e) => {
-    console.log('input text value : ' + e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.props);
+    this.props.onCreate(this.state);
   }
 
   render() {
     return (
-      <form>
-        <input placeholder="이름" onChange={this.handleChange} />
-
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="이름" name="name" onChange={this.handleChange} />
+        <input placeholder="전화번호" name="phone" onChange={this.handleChange} />
+        <button type="submit">등록</button>
+        <div>{this.state.name} {this.state.phone}</div>
       </form>
     ) 
   }
