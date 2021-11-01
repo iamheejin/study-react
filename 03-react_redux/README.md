d13510b3ac6ce596e714db184516a3e29dc723dd
iamheejin <adkj777@gmail.com>
2021-11-01 15:55:08 +0900

    리덕스 샘플 프로젝트 clone

--- 03-react_redux/README.md
+++ 03-react_redux/README.md
@@ -4,6 +4,10 @@ react + redux 학습
 
 참고 : https://velopert.com/3528
 
+$ git clone https://github.com/vlpt-playground/begin-redux.git
+
+리덕스 템플릿 프로젝트를 클론하여 사용.
+
 ## 진행 일정
 
 ### 11월 1주차 : 블로그 강의 학습 및 실습 따라하기
--- 03-react_redux/src/App.js
+++ 03-react_redux/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  id = 3;
+  state={
+    input:'',
+    todos:[
+      { id:0, text:'리액트 소개', checked: false},
+      { id:1, text:'리액트 사용법', checked: true},
+      { id:2, text:'리액트 활용', checked: false}
+    ]
+  }
+  
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+    console.log(input);
+    this.setState({
+      input:'',
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false
+      })
+    })
+  }
+
+  handleKeyPress = (e) => {
+    if(e.key === 'Enter'){
+      this.handleCreate();
+    }
+  }
+
+  handleToggle = (id) => {
+    console.log('toggle id:'+id)
+    const {todos} = this.state;
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index];
+    const nextTodos = [...todos];
+
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked
+    }
+
+    this.setState({
+      todos: nextTodos
+    })
+  }
+
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    })
+  }
+
+  render(){
+    const{ handleChange, handleCreate, handleKeyPress, handleToggle, handleRemove } = this;
+    const{ input, todos } = this.state;
+    return (
+      <TodoListTemplate form={<Form value={input} onChange={handleChange} onCreate={handleCreate} onKeyPress={handleKeyPress}/>}>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+      </TodoListTemplate>
+    )
+  }
 }
 
 export default App;
