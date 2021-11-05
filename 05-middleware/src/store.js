67768da0ebb6dde35fa26d6de526f5adfd700735
iamheejin <adkj777@gmail.com>
2021-11-05 17:59:43 +0900

    redux-thunk 미들웨어로 1초 딜레이 액션 함수 생성

--- 05-middleware/src/store.js
+++ 05-middleware/src/store.js
@@ -2,17 +2,19 @@ import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
 
 
-//redux-devtool-extension
+//1.redux-devtool-extension
 //import { composeWithDevTools } from 'redux-devtools-extension';
 //const store = createStore(modules, composeWithDevTools());
 
-//직접 작성한 middleware
+//2.직접 작성한 middleware
 //import loggerMiddleware from './lib/loggerMiddleware';
 //const store = createStore(modules, applyMiddleware(loggerMiddleware));
 
-//redux-logger 라이브러리
+//3.redux-logger 라이브러리
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+
 const logger = createLogger();
-const store = createStore(modules, applyMiddleware(logger));
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk));
 
 export default store;
